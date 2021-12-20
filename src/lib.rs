2ba0986463339326099a15dc5d7ecf5d5842b91a
michaelrauh <michaelrauh@ymail.com>
2021-12-19 19:12:56 -0500

    first thoughts on forming vocabulary

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-pub fn clean_sentences(sentences: String) -> Vec<Vec<String>> {
+use std::collections::HashSet;
 
+pub fn clean_sentences(sentences: String) -> Vec<Vec<String>> {
     sentences
         .split(|x| x == '.' || x == '!' || x == '?')
         .filter(|x| !x.is_empty())
@@ -17,6 +18,16 @@ pub fn clean_sentences(sentences: String) -> Vec<Vec<String>> {
         .collect()
 }
 
+fn make_vocabulary(sentences: String) -> HashSet<String> {
+    let hashes: Vec<HashSet<String>> = clean_sentences(sentences)
+        .iter()
+        .map(|x| x.iter().cloned().collect())
+        .collect();
+    hashes
+        .iter()
+        .fold(HashSet::new(), |acc, x| acc.union(x).cloned().collect())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,5 +42,21 @@ mod tests {
                 vec!("f".to_string(), "g".to_string())
             )
         );
+    }
+
+    #[test]
+    fn make_vocabulary_makes_a_set_of_all_words() {
+        assert_eq!(
+            make_vocabulary("a b. c d. b c.".to_string()),
+            [
+                "a".to_string(),
+                "b".to_string(),
+                "c".to_string(),
+                "d".to_string()
+            ]
+            .iter()
+            .cloned()
+            .collect()
+        )
     }
 }
