a2b182f7053bd516ada9a952d31958c9856616e7
michaelrauh <michaelrauh@ymail.com>
2022-01-15 16:23:10 -0500

    remove backward phrases

--- src/config.rs
+++ src/config.rs
@@ -43,8 +43,7 @@ pub struct Config {
     vocabulary: IntSet<usize>,
     forward: IntMap<usize, IntSet<usize>>,
     backward: IntMap<usize, IntSet<usize>>,
-    backward_phrases: Trie, // todo delete this
-    forward_phrases: Trie,
+    phrases: Trie,
 }
 
 impl Config {
@@ -60,12 +59,8 @@ impl Config {
         self.vocabulary.iter()
     }
 
-    pub fn get_backward_phrases(&self) -> &Trie {
-        &self.backward_phrases
-    }
-
-    pub fn get_forward_phrases(&self) -> &Trie {
-        &self.forward_phrases
+    pub fn get_phrases(&self) -> &Trie {
+        &self.phrases
     }
 
     pub fn from_sentences(raw: String) -> (Config, StringInterner) {
@@ -81,10 +76,10 @@ impl Config {
         let mut vocabulary = IntSet::default();
         let mut forward = IntMap::default();
         let mut backward = IntMap::default();
-        let mut backward_phrases = Trie::new();
-        let mut forward_phrases = Trie::new();
+        let mut phrases = Trie::new();
 
         for sentence in sentences {
+            let mut current_trie = &mut phrases;
             for i in 0..sentence.len() - 1 {
                 let word = sentence[i];
                 let next_word = sentence[i + 1];
@@ -100,12 +95,6 @@ impl Config {
                     .entry(next_word)
                     .or_insert_with(IntSet::default)
                     .insert(word);
-            }
-
-            let mut current_trie = &mut backward_phrases;
-            for i in (1..(sentence.len())).rev() {
-                let word = sentence[i];
-                let next_word = sentence[i - 1];
 
                 current_trie
                     .get
@@ -117,29 +106,12 @@ impl Config {
 
                 current_trie = current_trie.get.get_mut(&word).unwrap();
             }
-
-            let mut other_trie = &mut forward_phrases;
-            for i in 0..(sentence.len() - 1) {
-                let word = sentence[i];
-                let next_word = sentence[i + 1];
-
-                other_trie
-                    .get
-                    .entry(word)
-                    .or_insert_with(|| Trie::new())
-                    .get
-                    .entry(next_word)
-                    .or_insert_with(|| Trie::new());
-
-                other_trie = other_trie.get.get_mut(&word).unwrap();
-            }
         }
         Config {
             vocabulary,
             forward,
             backward,
-            backward_phrases,
-            forward_phrases,
+            phrases,
         }
     }
 }
@@ -149,7 +121,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_iterates() {
-        let (config, interner) = Config::from_sentences("a b. c d. a c. b d.".to_string());
+        let (config, _interner) = Config::from_sentences("a b. c d. a c. b d.".to_string());
         assert_eq!(config.vocabulary.len(), 4);
         assert_eq!(config.iter().collect::<Vec<_>>().len(), 4);
         for word in config.iter() {
@@ -200,23 +172,10 @@ mod tests {
     fn it_hops_phrases_forward_returning_none_on_a_bad_hop() {
         let (config, interner) = Config::from_sentences("a b. c d. a c. b d.".to_string());
         assert!(config
-            .get_forward_phrases()
+            .get_phrases()
             .phrase_hop(interner.get("a").unwrap().to_usize())
             .unwrap()
             .phrase_hop(interner.get("b").unwrap().to_usize())
-            .unwrap()
-            .phrase_hop(interner.get("c").unwrap().to_usize())
-            .is_none());
-    }
-
-    #[test]
-    fn it_hops_phrases_backward_returning_none_on_a_bad_hop() {
-        let (config, interner) = Config::from_sentences("a b. c d. a c. b d.".to_string());
-        assert!(config
-            .get_backward_phrases()
-            .phrase_hop(interner.get("b").unwrap().to_usize())
-            .unwrap()
-            .phrase_hop(interner.get("a").unwrap().to_usize())
             .unwrap()
             .phrase_hop(interner.get("c").unwrap().to_usize())
             .is_none());
